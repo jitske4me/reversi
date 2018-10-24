bf04b4703af769a7cd1f3e4e69bd996dbe18a9b5
Koeleman, J.A.L. (Jitske) <j.a.l.koeleman@students.uu.nl>
2018-10-24 16:29:08 +0200

    Help functie is af. Zetten gelimiteerd tot een locatie waar het mag. Score panel aangemaakt en functioneert naar behoren. Comments toegevoegd en overbodige dingen weggehaald.

--- Reversi/Reversi_Scherm.Designer.cs
+++ Reversi/Reversi_Scherm.Designer.cs
@@ -31,6 +31,11 @@
             this.nieuw_spel = new System.Windows.Forms.Button();
             this.help = new System.Windows.Forms.Button();
             this.panel_speelveld = new System.Windows.Forms.Panel();
+            this.panel_score = new System.Windows.Forms.Panel();
+            this.label_status = new System.Windows.Forms.Label();
+            this.label_Nrood = new System.Windows.Forms.Label();
+            this.label_Nblauw = new System.Windows.Forms.Label();
+            this.panel_score.SuspendLayout();
             this.SuspendLayout();
             // 
             // nieuw_spel
@@ -62,16 +67,57 @@
             this.panel_speelveld.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_speelveld_Paint);
             this.panel_speelveld.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel_speelveld_MouseClick);
             // 
+            // panel_score
+            // 
+            this.panel_score.Controls.Add(this.label_Nblauw);
+            this.panel_score.Controls.Add(this.label_Nrood);
+            this.panel_score.Controls.Add(this.label_status);
+            this.panel_score.Location = new System.Drawing.Point(542, 196);
+            this.panel_score.Name = "panel_score";
+            this.panel_score.Size = new System.Drawing.Size(177, 279);
+            this.panel_score.TabIndex = 3;
+            this.panel_score.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_score_Paint);
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(57, 25);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(46, 17);
+            this.label_status.TabIndex = 0;
+            this.label_status.Text = "label1";
+            // 
+            // label_Nrood
+            // 
+            this.label_Nrood.AutoSize = true;
+            this.label_Nrood.Location = new System.Drawing.Point(80, 113);
+            this.label_Nrood.Name = "label_Nrood";
+            this.label_Nrood.Size = new System.Drawing.Size(46, 17);
+            this.label_Nrood.TabIndex = 1;
+            this.label_Nrood.Text = "label1";
+            // 
+            // label_Nblauw
+            // 
+            this.label_Nblauw.AutoSize = true;
+            this.label_Nblauw.Location = new System.Drawing.Point(80, 172);
+            this.label_Nblauw.Name = "label_Nblauw";
+            this.label_Nblauw.Size = new System.Drawing.Size(46, 17);
+            this.label_Nblauw.TabIndex = 2;
+            this.label_Nblauw.Text = "label1";
+            // 
             // reversi_scherm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(777, 539);
+            this.Controls.Add(this.panel_score);
             this.Controls.Add(this.panel_speelveld);
             this.Controls.Add(this.help);
             this.Controls.Add(this.nieuw_spel);
             this.Name = "reversi_scherm";
             this.Text = "Form1";
+            this.panel_score.ResumeLayout(false);
+            this.panel_score.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -81,5 +127,9 @@
         private System.Windows.Forms.Button nieuw_spel;
         private System.Windows.Forms.Button help;
         private System.Windows.Forms.Panel panel_speelveld;
+        private System.Windows.Forms.Panel panel_score;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Label label_Nrood;
+        private System.Windows.Forms.Label label_Nblauw;
     }
 }
--- Reversi/Reversi_Scherm.cs
+++ Reversi/Reversi_Scherm.cs
@@ -73,6 +73,9 @@ namespace Reversi
 
         public void panel_speelveld_Paint(object sender, PaintEventArgs e)
         {
+
+            bool[,] possible_moves = help_functie(turn, bezetting);
+            
             //// Declareren van maten hele speelveld en de vakken;
             // We nemen de remainder van de deling zodat de grootte van de vakken bij elkaar niet groter zijn dat het hele veld
             hoogte = panel_speelveld.Height - (panel_speelveld.Height % vert_vak);
@@ -81,6 +84,8 @@ namespace Reversi
             vakhoogte = hoogte / vert_vak;
 
             Pen pen = new Pen(Brushes.Black);
+            Pen pencircle = new Pen(Brushes.Black,3);
+            Pen penhulp = new Pen(Brushes.Black);
 
             // Teken de vakken/het rooster met een for loop
             for (int t = 0; t <= hor_vak; t++)
@@ -95,16 +100,16 @@ namespace Reversi
                     if (bezetting[teken_hor, teken_vert] == 1)
                     {
                         e.Graphics.FillEllipse(kleur1, teken_hor * vakbreedte, teken_vert * vakhoogte, vakbreedte, vakhoogte);
-                        e.Graphics.DrawEllipse(pen, teken_hor * vakbreedte, teken_vert * vakhoogte, vakbreedte, vakhoogte);
-                        e.Graphics.DrawEllipse(pen, (teken_hor * vakbreedte) + (vakbreedte * 1 / 4),
+                        e.Graphics.DrawEllipse(pencircle, teken_hor * vakbreedte, teken_vert * vakhoogte, vakbreedte, vakhoogte);
+                        e.Graphics.DrawEllipse(pencircle, (teken_hor * vakbreedte) + (vakbreedte * 1 / 4),
                                                     (teken_vert * vakhoogte) + (vakhoogte * 1 / 4),
                                                     vakbreedte / 2, vakhoogte / 2);
                     }
                     else if (bezetting[teken_hor, teken_vert] == 2)
                     {
                         e.Graphics.FillEllipse(kleur2, teken_hor * vakbreedte, teken_vert * vakhoogte, vakbreedte, vakhoogte);
-                        e.Graphics.DrawEllipse(pen, teken_hor * vakbreedte, teken_vert * vakhoogte, vakbreedte, vakhoogte);
-                        e.Graphics.DrawEllipse(pen, (teken_hor * vakbreedte) + (vakbreedte * 1 / 4),
+                        e.Graphics.DrawEllipse(pencircle, teken_hor * vakbreedte, teken_vert * vakhoogte, vakbreedte, vakhoogte);
+                        e.Graphics.DrawEllipse(pencircle, (teken_hor * vakbreedte) + (vakbreedte * 1 / 4),
                                                     (teken_vert * vakhoogte) + (vakhoogte * 1 / 4),
                                                     vakbreedte / 2, vakhoogte / 2);
                     }
@@ -113,11 +118,59 @@ namespace Reversi
                     if (help_aan == 1)
                     {
                         if (possible_moves[teken_hor, teken_vert] == true)
-                            e.Graphics.DrawEllipse(pen, (teken_hor * vakbreedte) + (vakbreedte * 1 / 4),
+                            e.Graphics.DrawEllipse(penhulp, (teken_hor * vakbreedte) + (vakbreedte * 1 / 4),
                                                         (teken_vert * vakhoogte) + (vakhoogte * 1 / 4),
                                                         vakbreedte / 2, vakhoogte / 2);
                     }                                                                                              
                 }
+        }
+        
+        // Teken het panel met de status & score
+        private void panel_score_Paint(object sender, PaintEventArgs e)
+        {
+            string status = "";
+            int Nrood = 0;
+            int Nblauw = 0;
+            bool game_active = false;
+
+            // Tel hoeveel rode en blauwe stenen er zijn en kijk of het veld vol is
+            for (int x = 0; x < hor_vak; x++)
+                for (int y = 0; y < vert_vak; y++)
+                    if (bezetting[x, y] == 1)
+                        Nrood++;
+                    else if (bezetting[x, y] == 2)
+                        Nblauw++;
+                    else if (bezetting[x, y] == 0)
+                        game_active = true;
+
+            // Wiens zet is het?
+            if (game_active)
+            {
+                if (turn == 1)
+                    status = "Rood aan zet";
+                else if (turn == 2)
+                    status = "Blauw aan zet";
+            }
+
+            // In het geval dat het veld vol is, kijk wie er heeft gewonnen (of remise)
+            else if (!game_active)
+            {
+                if (Nrood > Nblauw)
+                    status = "Rood wint";
+                else if (Nblauw > Nrood)
+                    status = "Blauw wint";
+                else if (Nblauw == Nrood)
+                    status = "Remise";
+            }
+
+            // Tekst aanpassen
+            label_status.Text = status;
+            label_Nrood.Text = Nrood.ToString() + " stenen";
+            label_Nblauw.Text = Nblauw.ToString() + " stenen";
+
+            // Draw red/blue circles for the score
+            e.Graphics.FillEllipse(Brushes.Red, 40, 105, 30, 30);
+            e.Graphics.FillEllipse(Brushes.Blue, 40, 165, 30, 30);
         }
 
         //BEDIENING VAN BUTTONS & PANEL
@@ -127,19 +180,22 @@ namespace Reversi
         {
             NewGame();
             panel_speelveld.Invalidate();
+            panel_score.Invalidate();
         }
 
         // Aan en uitzetten helpfunctie
         private void help_MouseClick(object sender, MouseEventArgs e)
-        {
-            if (help_aan == 0)
-                help_aan = 1;
-            else if (help_aan == 1)
-                help_aan = 0;
+        {            
+                if (help_aan == 0)
+                    help_aan = 1;
+                else if (help_aan == 1)
+                    help_aan = 0;
 
             panel_speelveld.Invalidate();
+            panel_score.Invalidate();
+
         }
-                     
+
 
         private int horloc;
         private int vertloc;
@@ -150,18 +206,15 @@ namespace Reversi
             // int horloc = MousePosition.X/hor_vak ;
 
             horloc = e.Location.X / vakbreedte;
-            vertloc = e.Location.Y / vakhoogte  ;
+            vertloc = e.Location.Y / vakhoogte;
 
             // Kijk of de zet klopt
-
-            // Deze is outdated. We moeten eigenlijk doen dat ie checkt of een bepaalde loc in volgende_zet zit
-            bool valid = legal_move(turn, bezetting, horloc, vertloc); 
 
             // Maakt een array met welke locaties wel en niet mogen
             bool[,] volgende_zet = help_functie(turn, bezetting);
 
             // Als de zet klopt, pas dit aan
-            if (valid)
+            if (volgende_zet[horloc, vertloc])
             {
                 // Change turn
                 if (turn == 1)
@@ -179,33 +232,13 @@ namespace Reversi
                     Console.WriteLine("error");
                     bezetting[horloc, vertloc] = -1;
                 }
-            }
-            
-            panel_speelveld.Invalidate();
+
+                panel_speelveld.Invalidate();
+                panel_score.Invalidate();
+            }               
         }
 
         // IS DE ZET GELDIG?
-
-        // Kijk of de zet mag (dit is alleen of er al iets staat of nog niet)
-        bool legal_move(int turn, int[,] array, int xpos, int ypos)
-        {
-            bool valid_move = false;
-            if (bezetting[xpos, ypos] != 0)
-            {
-                return valid_move;
-            }
-            else if(bezetting[xpos,ypos]==0)
-            {
-                // Check hier of je hor/vert/diag1/diag2 een andere steen insluit
-                valid_move = true;
-                return valid_move;
-            }
-            else
-            {
-                return valid_move;
-            }
-
-        }
 
         //  Maakt een array met welke locaties wel en niet mogen
         bool[,] help_functie(int turn, int[,] array)
@@ -223,8 +256,8 @@ namespace Reversi
                 tegenstander = 1;
 
             // Loop alle x en y af
-            for (int x = 0; x < hor_index; x++)
-                for (int y = 0; y < vert_index; y++)
+            for (int x = 0; x <= hor_index; x++)
+                for (int y = 0; y <= vert_index; y++)
                 {
                     // Check of de een vakje leeg is of bezet
                     if (array[x, y] != 0)
@@ -298,24 +331,119 @@ namespace Reversi
                                         ;
                                 }
 
-                        //// Diagonaal linksboven naar rechtsonder \>
-                        //if (x < hor_index && y < vert_index)
-                        //    if (bezetting[x + 1, y + 1] == tegenstander)
-                        //    {
-                        //        int tx = x + 1;
-                        //        int ty = y + 1;
-                                
-                        //        while (tx < hor_index && ty < vert_index)
-                        //        {
-                
-                        //        }
-                        //    }
+                        // Diagonaal linksboven naar rechtsonder
+                        if (x < hor_index && y < vert_index)
+                            if (bezetting[x + 1, y + 1] == tegenstander)
+                            {
+                                int tx = x + 1;
+                                int ty = y + 1;
 
+                                while (tx <= hor_index && ty <= vert_index)
+                                {
+                                    if (bezetting[tx, ty] == speler)
+                                    {
+                                        possible_moves[x, y] = true;
+                                        tx = hor_vak;
+                                        ty = vert_vak;
+                                    }
+                                    else if (bezetting[tx, ty] == 0)
+                                    {
+                                        tx = hor_vak;
+                                        ty = vert_vak;
+                                    }
+                                    else if (bezetting[tx, ty] == tegenstander)
+                                        ;
 
+                                    tx++;
+                                    ty++;
+                                }
+                            }
+
+                        // Diagonaal rechtsonder naar linksboven
+                        if (x > 0 && y > 0)
+                            if (bezetting[x - 1, y - 1] == tegenstander)
+                            {
+                                int tx = x - 1;
+                                int ty = y - 1;
+
+                                while (tx >= 0 && ty >= 0)
+                                {
+                                    if (bezetting[tx, ty] == speler)
+                                    {
+                                        possible_moves[x, y] = true;
+                                        tx = 0;
+                                        ty = 0;
+                                    }
+                                    else if (bezetting[tx, ty] == 0)
+                                    {
+                                        tx = 0;
+                                        ty = 0;
+                                    }
+                                    else if (bezetting[tx, ty] == tegenstander)
+                                        ;
+
+                                    tx--;
+                                    ty--;
+                                }
+                            }
+
+                        // Diagonaal linksonder naar rechstboven 
+                        if (x < hor_index && y > 0)
+                            if (bezetting[x + 1, y - 1] == tegenstander)
+                            {
+                                int tx = x + 1;
+                                int ty = y - 1;
+
+                                while (tx <= hor_index && ty >= 0)
+                                {
+                                    if (bezetting[tx, ty] == speler)
+                                    {
+                                        possible_moves[x, y] = true;
+                                        tx = hor_vak;
+                                        ty = 0;
+                                    }
+                                    else if (bezetting[tx, ty] == 0)
+                                    {
+                                        tx = hor_vak;
+                                        ty = 0;
+                                    }
+                                    else if (bezetting[tx, ty] == tegenstander)
+                                        ;
+
+                                    tx++;
+                                    ty--;
+                                }
+                            }
+
+                        // Diagonaal linksonder naar rechstboven 
+                        if (x > 0 && y < vert_index)
+                            if (bezetting[x - 1, y + 1] == tegenstander)
+                            {
+                                int tx = x - 1;
+                                int ty = y + 1;
+
+                                while (tx >= 0 && ty <= vert_index)
+                                {
+                                    if (bezetting[tx, ty] == speler)
+                                    {
+                                        possible_moves[x, y] = true;
+                                        tx = 0;
+                                        ty = vert_vak;
+                                    }
+                                    else if (bezetting[tx, ty] == 0)
+                                    {
+                                        tx = 0;
+                                        ty = vert_vak;
+                                    }
+                                    else if (bezetting[tx, ty] == tegenstander)
+                                        ;
+
+                                    tx--;
+                                    ty++;
+                                }
+                            }
 
                     }
-
-
                 }
 
             return possible_moves;
